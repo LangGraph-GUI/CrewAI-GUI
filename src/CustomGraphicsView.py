8cb85a40f55b8187db75c2e8ce271d36d09b6d99
HomunMage <homun@posetmage.com>
2024-05-31 16:02:44 +0800

    right click drag canvas

--- src/CustomGraphicsView.py
+++ src/CustomGraphicsView.py
@@ -1,3 +1,5 @@
+# CustomGraphicsView.py
+
 from PySide6.QtWidgets import QGraphicsView
 from PySide6.QtCore import Qt, QPointF
 
@@ -5,27 +7,13 @@ class CustomGraphicsView(QGraphicsView):
     def __init__(self, scene):
         super().__init__(scene)
         self.setDragMode(QGraphicsView.NoDrag)
-        self._space_pressed = False
         self._dragging = False
         self._last_mouse_pos = QPointF()
 
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Space:
-            self._space_pressed = True
-        else:
-            super().keyPressEvent(event)
-
-    def keyReleaseEvent(self, event):
-        if event.key() == Qt.Key_Space:
-            self._space_pressed = False
-        else:
-            super().keyReleaseEvent(event)
-
     def mousePressEvent(self, event):
-        if self._space_pressed:
+        if event.button() == Qt.RightButton:
             self._dragging = True
             self._last_mouse_pos = event.pos()
-            self.setCursor(Qt.ClosedHandCursor)
         else:
             super().mousePressEvent(event)
 
@@ -39,7 +27,7 @@ class CustomGraphicsView(QGraphicsView):
             super().mouseMoveEvent(event)
 
     def mouseReleaseEvent(self, event):
-        if self._dragging:
+        if event.button() == Qt.RightButton and self._dragging:
             self._dragging = False
             self.setCursor(Qt.ArrowCursor)
         else:
