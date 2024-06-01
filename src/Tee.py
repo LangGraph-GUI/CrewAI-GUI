60686976c62cbe378f576081ed259e392c79992c
HomunMage <homun@posetmage.com>
2024-06-01 21:34:29 +0800

    log with time

--- src/Tee.py
+++ src/Tee.py
@@ -1,14 +1,18 @@
 import sys
+import datetime
 
 class Tee:
-    def __init__(self, filename, mode='w'):
+    def __init__(self, filename, mode='a'):  # Use 'a' for append mode
         self.file = open(filename, mode)
         self.stdout = sys.stdout
         sys.stdout = self
 
     def write(self, message):
-        self.stdout.write(message)
-        self.file.write(message)
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        message_with_timestamp = f"{timestamp} - {message}"
+        self.stdout.write("\n")
+        self.stdout.write(message_with_timestamp)
+        self.file.write(message_with_timestamp)
         self.file.flush()
 
     def flush(self):
